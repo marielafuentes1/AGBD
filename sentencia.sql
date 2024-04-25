30fe81e3ec6dda16e4721acde3a9b403b5adde70
mariela <mf44875939@gmail.com>
2024-04-25 12:07:33 -0300

    completando sql mistery

--- sentencia.sql
+++ sentencia.sql
@@ -2,4 +2,4 @@ SELECT * FROM INHABITANT
 select * FROM INHABITANT WHERE STATE = 'friendly'
 select * FROM INHABITANT WHERE STATE = 'friendly' AND JOB = "weaponsmith"
 select * FROM INHABITANT WHERE STATE = 'friendly' AND JOB LIKE '%smith'
- SELECT personid FROM inhabitant WHERE name = "Stranger"
+  "StrSELECT personid FROM inhabitant WHERE name =anger"
---
+++ sql muder mistery.sql
@@ -0,0 +1,5 @@
+SELECT * FROM crime_scene_report
+WHERE  date = '20180115' AND type = 'murder' AND city = 'SQL City'
+SELECT name from person
+WHERE name like'Annabel%'
+AND address_street_name = 'Franklin Ave'
