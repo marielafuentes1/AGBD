f0cb269447f050abefef4db51082fd3ee9492762
mariela <mf44875939@gmail.com>
2024-05-30 09:57:20 -0300

    hice el ejercicio 1 y2

---
+++ EjercicioModificacion.sql
@@ -0,0 +1,8 @@
+1) INSERT INTO  artists (name)
+   VALUES ("Selena Gomez");
+
+2) INSERT INTO albums (Title, ArtistId)
+   VALUES ("Rare",276)
+
+3)   INSERT INTO tracks (name, Milliseconds, UnitPrice, MediaTypeId)
+     VALUES ("Ring" , " Vulnerable", "Rare" )
