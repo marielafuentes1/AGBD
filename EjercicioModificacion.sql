c9e0a03f7e818c50b307aefd82254eec7a663623
mariela <mf44875939@gmail.com>
2024-05-30 11:43:53 -0300

    hice los tres ejercicios

--- EjercicioModificacion.sql
+++ EjercicioModificacion.sql
@@ -5,4 +5,12 @@
    VALUES ("Rare",276)
 
 3)   INSERT INTO tracks (name, Milliseconds, UnitPrice, MediaTypeId)
-     VALUES ("Ring" , " Vulnerable", "Rare" )
+     VALUES ("Ring" ,3 , 137400,1.99) ,(" Vulnerable",3,187200,1.99), ("Rare", 3,204600,1.99)
+
+     UPDATE tracks SET AlbumId = 348, Composer = "Selena Gomez"
+     WHERE GenreId is NULL;
+
+
+DELETE FROM tracks WHERE AlbumId = 348; 
+DELETE FROM albums WHERE ArtistId = 276;
+DELETE FROM artists WHERE ArtistId = 276;
